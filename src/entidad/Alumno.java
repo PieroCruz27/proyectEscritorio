05a3919e966f74467180840bc1cdbc7bfe5c008d
lvallash <leticiavh_3@hotmail.com>
2022-04-24 22:44:12 -0500

    Clase Alumno

---
+++ src/entidad/Alumno.java
@@ -0,0 +1,51 @@
+package entidad;
+
+public class Alumno {
+
+	//Vallas Herencia
+	
+	private String nombre;
+	private String apellido;
+	private int dni;
+	private String pais;
+	private String correo;
+	private String fechanac;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getFechanac() {
+		return fechanac;
+	}
+	public void setFechanac(String fechanac) {
+		this.fechanac = fechanac;
+	}
+	
+	
+}
